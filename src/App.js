694445fa3a0c5fd2d4612696e9cbe7625b06fe76
Rabiul Islam <rabiul.dev@gmail.com>
2022-05-21 00:14:47 +0600

    create bike data state and set data by fetching in useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  
+  // BIKES DATA STORE//
+  const [dreamBike, setDreamBike] = useState([]);
+
+  // FETCH THE BIKES DATA//
+  useEffect(()=>{
+    fetch("fakeData.json")
+    .then(res => res.json())
+    .then(data => setDreamBike(data))
+  },[])
+
+
   return (
     <div className="App">
-        
+        {dreamBike.length}
     </div>
   );
 }
