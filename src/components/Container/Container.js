9d6e5c5cde8adefeb6494ee3ec7b92555c7d96a3
Rabiul Islam <rabiul.dev@gmail.com>
2022-05-22 17:26:38 +0600

    create get random number fucntion and create dream bike store

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import "./Container.css"
 import { useEffect, useState } from 'react';
+import "./Container.css"
 import Bike from '../Bike/Bike';
 import Cart from '../Cart/Cart';
 
@@ -9,7 +9,14 @@ const Container = () => {
 
   // BIKES DATA STORE//
   const [dreamBikes, setDreamBikes] = useState([]);
+
+  // ADDED TO CART BIKES DATA STORE//
   const [addedBikes, setAddedBikes] = useState([]);
+
+  // THE DREAM BIKE STORE//
+  const [dreamBike, setDreamBike] = useState([]);
+
+  // CONDITION VALUE FOR CART ITEMS//
   const [moreThanFour, setMoreThanFour] = useState(false)
 
   // FETCH THE BIKES DATA//
@@ -19,62 +26,76 @@ const Container = () => {
       .then(data => setDreamBikes(data))
   }, [])
 
-  // HANDLE ADD TO CART BUTTON//
+
+  // START HANDLE ADD TO CART BUTTON//
   const addToCartButton = (bike) => {
 
     if (addedBikes.length === 0) {
-          setAddedBikes([bike])
+      setAddedBikes([bike])
+    }
+    else if (addedBikes.length < 4) {
+      for (const addedBike of addedBikes) {
+        const exist = addedBike;
+        const previousState = addedBikes.filter(addedBike => addedBike.id !== bike.id)
+        if (exist.id !== bike.id) {
+          setAddedBikes([...previousState, bike])
         }
-    
-    else if(addedBikes.length < 4) {
-        for (const addedBike of addedBikes ){
-          const exist = addedBike;
-          const previousState = addedBikes.filter(addedBike => addedBike.id !== bike.id)
-          if(exist.id !== bike.id ){
-            setAddedBikes([bike, ...previousState ])
-          }
-        }
-       
+      }
     }
     else {
       setMoreThanFour(true)
     }
   }
+  //END HANDLE ADD TO CART BUTTON//
 
-  //GENERATE RANDOM NUMBER//
-  const getRandomNumber = () => Math.floor(Math.random() * 12);
+  //-------!!!!!!!!!!!!!!!!!!!!!!!!-------//
 
-  // HANDLE GET THE DREAM BIKES BUTTON//
-  const hanleDreamBikeBtn = () => {
-    const radomNumber = getRandomNumber();
-    if (addedBikes) {
-      const luckyBike = addedBikes.find(addedBike => addedBike.id === radomNumber)
-      console.log(luckyBike)
-    }
-    else {
-      hanleDreamBikeBtn()
-    }
-
+  //GET RANDOM NUMBER FUNCTION START//
+  const getRandomNum = (min, max) => {
+    const rangeNum = max - min + 1;
+    const randomNum = Math.random() * rangeNum;
+    const result = Math.floor(randomNum) + min
+    return result;
   }
 
 
+  //GET RANDOM NUMBER FUNCTION END//
+
+  //-------!!!!!!!!!!!!!!!!!!!!!!!!-------//
+
+  // START HANDLE GET THE DREAM BIKES BUTTON//
+  const hanleDreamBikeBtn = () => {
+    if (addedBikes.length !== 0) {
+      const index = getRandomNum(0, addedBikes.length - 1)
+      setDreamBike(addedBikes[index])
+
+    }
+  }
+
+  // END HANDLE GET THE DREAM BIKES BUTTON//
+
   return (
-    <div className='container'>
-      <div className="bikes-container">
-        <h1 className='title'>Welcome to Your Dream Bikes World!!!!!!</h1>
-        <div className="bike-shocase">
-          {
-            dreamBikes.map(bike => <Bike key={bike.id} bike={bike} addToCartButton={addToCartButton}></Bike>)
-          }
-        </div>
-      </div>
-      <div className="cart-container">
-        <div className="position-relative">
-          <div className="cart">
-            <Cart addedBikes={addedBikes} moreThanFour={moreThanFour} hanleDreamBikeBtn={hanleDreamBikeBtn} />
+    <div>
+      {
+        console.log(dreamBike)
+      }
+      <div className='container'>
+        <div className="bikes-container">
+          <h1 className='title'>Welcome to Your Dream Bikes World!!!!!!</h1>
+          <div className="bike-shocase">
+            {
+              dreamBikes.map(bike => <Bike key={bike.id} bike={bike} addToCartButton={addToCartButton}></Bike>)
+            }
           </div>
         </div>
+        <div className="cart-container">
+          <div className="position-relative">
+            <div className="cart">
+              <Cart addedBikes={addedBikes} moreThanFour={moreThanFour} hanleDreamBikeBtn={hanleDreamBikeBtn} />
+            </div>
+          </div>
 
+        </div>
       </div>
     </div>
   )
